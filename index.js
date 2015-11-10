ef8d339ad9bf3a6fdfcd4c77e157eb11911f6e30
Harry Boyes <harry.boyes@gmail.com>
2015-11-10 10:54:30 +0000

    Changed to port 8080

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
-var express = require('express');
 
 app.get('/', function(req, res){
 	res.sendfile('index.html');
@@ -16,45 +15,20 @@ app.get('/gameClass.js', function (req, res) {
     res.sendfile(__dirname + '\\gameClass.js');
 });
 
-app.use(express.static('public'));
-
-var sockets = {};
-var teams = [0, 1];
+var mySocket;
 
 io.on('connection', function(socket){
-	var socketId;
-	
+	console.log('a user has connected');
 	socket.on('disconnect', function(){
-		if(sockets[socketId])
-			teams.push(sockets[socketId].team);
-		
-		delete sockets[socketId];
+		console.log('a user has disconnected');
+	});
+	socket.on('message', function(msg){
+		console.log(msg);
 	});
 	
-	socket.on('init', function(msg){
-		socketId = msg.id;
-		
-		console.log("connected", msg.id);
-		
-		var team = teams.splice(Math.floor(Math.random() * teams.length), 1)[0];
-		
-		socket.emit('init', {team: team});
-		
-		sockets[socketId] = {team: team, s: socket};
-	});
-	
-	socket.on('update', function(message){
-		message.event = 'socket ' + message.event;
-		
-		for(var s in sockets)
-			if(sockets[s].s !== socket)
-			{
-				console.log("Emit", s, message);
-				sockets[s].s.emit('update', message);
-			}
-	});
+	mySocket = socket;
 });
 
-http.listen(3000, function(){
-	console.log('listening on *:3000');
+http.listen(8080, function(){
+	console.log('listening on *:8080');
 });
